10d330676cfd1b06e031bf318ebb1328dd49feb9
Melvin Gerritsen <melvin.gerritsen@ns.nl>
2024-03-11 13:29:03 +0100

    Test has been written but the scroll function does not work

--- app/src/androidTest/java/melvin/gerritsen/myrecipeapp/ComposeTest.kt
+++ app/src/androidTest/java/melvin/gerritsen/myrecipeapp/ComposeTest.kt
@@ -5,9 +5,11 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.hasTextExactly
+import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.compose.rememberNavController
 import melvin.gerritsen.myrecipeapp.ui.theme.MyRecipeAppTheme
 import org.junit.Rule
@@ -39,15 +41,37 @@ class MyComposeTest {
 //            composeTestRule.temp_wait_function()
         composeTestRule.waitUntilExactlyOneExists(hasText("meat", true))
         composeTestRule.onNodeWithText("meat", true).assertIsDisplayed()
+    }
 
+        @OptIn(ExperimentalTestApi::class)
+        @Test
+        fun my_second_recipe_test() {
+            composeTestRule.setContent {
+                val navController = rememberNavController()
+                MyRecipeAppTheme {
+                    RecipeApp(navController = navController)
+                }
+            }
+
+//            composeTestRule.temp_wait_function()
+//        composeTestRule
+//            .onAllNodesWithContentDescription("Beef").assertAny(hasTestTag("Beef"))
+            composeTestRule.onNodeWithText("Breakfast").performScrollTo()
+            composeTestRule.waitUntilExactlyOneExists(hasTextExactly("Breakfast"))
+            composeTestRule.onNodeWithText("Breakfast").assertIsDisplayed()
+            composeTestRule.onNode(hasTestTag(Test_Tag_Front_Screen) and hasText("Breakfast"))
+                .performClick()
+//            composeTestRule.temp_wait_function()
+            composeTestRule.waitUntilExactlyOneExists(hasText("meal", true))
+            composeTestRule.onNodeWithText("meal", true).assertIsDisplayed()
 
     }
 }
 
 
-//private fun ComposeTestRule.temp_wait_function(duration: Long = 50_000) {
-//    waitUntil(duration) { true == false }
-//}
+private fun ComposeTestRule.temp_wait_function(duration: Long = 50_000) {
+  waitUntil(duration) { true == false }
+}
 
 
 
