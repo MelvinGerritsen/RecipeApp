56d244b3f13545cff2500695775d569e9ad1da50
Melvin Gerritsen <melvin.gerritsen@ns.nl>
2024-03-13 09:24:55 +0100

    I write the second test and it works!!

--- app/src/androidTest/java/melvin/gerritsen/myrecipeapp/ComposeTest.kt
+++ app/src/androidTest/java/melvin/gerritsen/myrecipeapp/ComposeTest.kt
@@ -1,18 +1,17 @@
 package melvin.gerritsen.myrecipeapp
 
-import android.util.Log
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.hasTextExactly
-import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
-import androidx.compose.ui.test.printToLog
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeUp
 import androidx.navigation.compose.rememberNavController
 import melvin.gerritsen.myrecipeapp.ui.theme.MyRecipeAppTheme
 import org.junit.Rule
@@ -46,38 +45,53 @@ class MyComposeTest {
         composeTestRule.onNodeWithText("meat", true).assertIsDisplayed()
     }
 
-        @OptIn(ExperimentalTestApi::class)
-        @Test
-        fun my_second_recipe_test() {
-            composeTestRule.setContent {
-                val navController = rememberNavController()
-                MyRecipeAppTheme {
-                    RecipeApp(navController = navController)
-                }
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun my_second_recipe_test() {
+        composeTestRule.setContent {
+            val navController = rememberNavController()
+            MyRecipeAppTheme {
+                RecipeApp(navController = navController)
             }
+        }
 
 //            composeTestRule.temp_wait_function()
 //        composeTestRule
 //            .onAllNodesWithContentDescription("Beef").assertAny(hasTestTag("Beef"))
-            composeTestRule.waitUntilExactlyOneExists(hasTextExactly("Beef"))
-            composeTestRule.onRoot().printToLog("Hanna")
-            composeTestRule.onNodeWithText("Breakfast").performScrollTo()
-            //lazy column
-            composeTestRule.onRoot().printToLog("Hanna2")
-            composeTestRule.waitUntilExactlyOneExists(hasTextExactly("Breakfast"))
-            composeTestRule.onNodeWithText("Breakfast").assertIsDisplayed()
-            composeTestRule.onNode(hasTestTag(Test_Tag_Front_Screen) and hasText("Breakfast"))
-                .performClick()
-//            composeTestRule.temp_wait_function()
-            composeTestRule.waitUntilExactlyOneExists(hasText("meal", true))
-            composeTestRule.onNodeWithText("meal", true).assertIsDisplayed()
 
+        composeTestRule.waitUntilExactlyOneExists(hasTextExactly("Beef"))
+//        scrollToItemInLazyColumn("Breakfast")
+        composeTestRule.onNodeWithTag(Test_Tag_LazyVerticalGrid).performTouchInput { swipeUp() }
+        //composeTestRule.temp_wait_function()
+        composeTestRule.onNodeWithText("Breakfast").performScrollTo().performClick()
+
+        //lazy column
+//        composeTestRule.onRoot().printToLog("Hanna2")
+//        composeTestRule.waitUntilExactlyOneExists(hasTextExactly("Breakfast"))
+//        composeTestRule.onNodeWithText("Breakfast").assertIsDisplayed()
+//        composeTestRule.onNode(hasTestTag(Test_Tag_Front_Screen) and hasText("Breakfast"))
+//            .performClick()
+//
+        composeTestRule.waitUntilExactlyOneExists(hasText("meal", true))
+//        composeTestRule.onNodeWithText("meal", true).assertIsDisplayed()
+//
+//
+//    }
+//
+//    fun scrollToItemInLazyColumn(itemText: String) {
+//        composeTestRule.onNodeWithContentDescription("Breakfast").performScrollTo()
+//        composeTestRule.waitForIdle()
+//        composeTestRule.onNodeWithText(itemText).performTouchInput { swipeUp() }
+//    }
     }
 }
 
+// Testtag voor lazy column
+
+
 
 //private fun ComposeTestRule.temp_wait_function(duration: Long = 50_000) {
-//  waitUntil(duration) { true == false }
+// waitUntil(duration) { true == false }
 //}
 
 
